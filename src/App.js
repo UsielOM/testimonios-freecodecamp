1024baafda61f83054d6ad12c5eed3546a40b702
UsielOM <usiel31197@outlook.es>
2022-07-18 18:57:17 -0500

    Interface funcionando

--- src/App.js
+++ src/App.js
@@ -9,7 +9,25 @@ function App() {
         
       <h1>Estos son mis pokemones favoritos: </h1>
       </div>
-      <Testimonio  />
+      <Testimonio 
+      name='Pikachu'
+      type='Trueno'
+      description='Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon más conocido de la historia, mayormente por ser el acompañante del protagonista del anime, Ash Ketchum y la mascota representante de la franquicia Pokémon. Es el Pokémon inicial de Pokémon Amarillo y Pokémon: Letsgo Pikachu'
+      img = 'pikachu'
+      />
+           <Testimonio 
+      name='Eevee'
+      type='Normal'
+      description='Eevee es un Pokémon de tipo normal introducido en la primera generación. Se caracteriza por ser el Pokémon con más evoluciones posibles, con ocho. Es el Pokémon inicial del rival en Pokémon Amarillo y en Pokémon: Lets Go, Pikachu! y del protagonista en Pokémon: Lets Go, Eevee!.'
+      img = 'eevee'
+      />
+
+<Testimonio 
+      name='Elekid'
+      type='Trueno'
+      description='Elekid es un Pokémon bebé de tipo eléctrico introducido en la segunda generación. Se trata de un Pokémon adelantado.'
+      img = 'elekid'
+      />
     </div>
   );
 }
--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -4,12 +4,12 @@ function Testimonio (props) {
   return (
     <div className='contenedor-testimonio  '>
       <img className='imagen-testimonio' 
-       src={require ('../assets/img/pokemon-pikachu.jpg')}
+       src={require (`../assets/img/pokemon-${props.img}.jpg`)}
        alt='Foto de Pikachu'/>
       <div className='contendor-texto-testimonio'>
-          <p className='nombre-testimonio'>Piakchu</p>
-          <p className='cargo-testimonio'>Pokemon tipo trueno</p>
-          <p className='texto-testimonio'>Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon más conocido de la historia, mayormente por ser el acompañante del protagonista del anime, Ash Ketchum y la mascota representante de la franquicia Pokémon. Es el Pokémon inicial de Pokémon Amarillo y Pokémon: Let's Go Pikachu!</p>
+          <p className='nombre-testimonio'>{props.name} </p>
+          <p className='cargo-testimonio'>Pokemon tipo: {props.type}</p>
+          <p className='texto-testimonio'>"{props.description}"</p>
       </div>
     </div>
     
