6a5d7eca8a1ece683637656c7ccab53645d99dd8
UsielOM <usiel31197@outlook.es>
2022-07-18 17:01:48 -0500

    Estructura componente testimonio terminada

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -6,7 +6,11 @@ function Testimonio (){
       <img className='imagen-testimonio'
        src={require ('../assets/img/pokemon-pikachu.jpg')}
        alt='Foto de Pikachu'/>
-
+      <div className='contendor-texto-testimonio'>
+          <p className='nombre-testimonio'>Piakchu</p>
+          <p className='cargo-testimonio'>Pokemon tipo trueno</p>
+          <p className='texto-testimonio'>Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon más conocido de la historia, mayormente por ser el acompañante del protagonista del anime, Ash Ketchum y la mascota representante de la franquicia Pokémon. Es el Pokémon inicial de Pokémon Amarillo y Pokémon: Let's Go Pikachu!</p>
+      </div>
     </div>
     
   );
